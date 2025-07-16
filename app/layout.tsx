555c046c89b9fb92ba19a52f6a49bf47dd05e9fd
Raziel Navarro <elrazand@gmail.com>
2025-07-15 21:52:06 -0500

    chore: update layout metadata and language settings in RootLayout component for Luigi's Burger

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "Luigi's Burger",
+  description: "Ordena YA!",
+  generator: "Luigi's Burger",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body>{children}</body>
     </html>
-  )
+  );
 }
